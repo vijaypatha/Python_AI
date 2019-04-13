b0a64983aef2494a765e0b10ad4e2e63e4b4e8a7
Patha <vipatha@amazon.com>
2019-04-12 19:34:24 -0700

    argsprase

--- image_classification/get_input_args.py
+++ image_classification/get_input_args.py
@@ -38,11 +38,15 @@ def get_input_args():
     Returns:
      parse_args() -data structure that stores the command line arguments object  
     """
-    # Create Parse using ArgumentParser
-    
+    # Create Parse using ArgumentParser (Or Intializing the parser)
+    parser = argparse.ArgumentParser()
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    
+    parser.add_argument('--dir', type=str, default='pet_images/', 
+                        help='path to folder of images')
+    parser.add_argument('--arch', default = 'vgg' )
+    parser.add_argument('--dogfile', default = 'dognames.txt' )
+
     
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
-    return None
+    return parser.parse_args()
