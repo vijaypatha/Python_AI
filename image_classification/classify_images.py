d5d82baf09c6295c7ab55b95f3947ec82060d4a4
Patha <vipatha@amazon.com>
2019-04-20 16:11:34 -0700

    classify

--- image_classification/classify_images.py
+++ image_classification/classify_images.py
@@ -75,7 +75,7 @@ def classify_images(images_dir, results_dic, model):
        #  Runs classifier function to classify the images classifier function 
        # inputs: path + filename  and  model, returns model_label 
        # as classifier label
-       model_label = key
+       model_label = classifier(images_dir+key, model)
 
        # TODO: 3b. BELOW REPLACE pass with CODE to process the model_label to 
        #           convert all characters within model_label to lowercase 
@@ -85,8 +85,8 @@ def classify_images(images_dir, results_dic, model):
        #
        # Processes the results so they can be compared with pet image labels
        # set labels to lowercase (lower) and stripping off whitespace(strip)
-       for labels in model_label:
-         model_label = labels.lower().strip()
+       model_label = model_label.lower()
+       model_label = model_label.strip()
               
        # defines truth as pet image label 
        truth = results_dic[key][0]
@@ -101,7 +101,7 @@ def classify_images(images_dir, results_dic, model):
        # as an exact match to on of the terms in the list - then they are added to 
        # results_dic as an exact match(1) using extend list function
        if truth in model_label:
-           results_dic.extend(model_label)
+         results_dic[key] = [truth, model_label, 1]
 
        # TODO: 3d. REPLACE pass BELOW with CODE that uses the extend list function
        #           to add the classifier label (model_label) and the value of
@@ -112,5 +112,4 @@ def classify_images(images_dir, results_dic, model):
        # if not found then added to results dictionary as NOT a match(0) using
        # the extend function 
        else:
-           pass
-    None 
+           results_dic[key] = [truth, model_label, 0]
